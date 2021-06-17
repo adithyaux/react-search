b95b4dff531f39b7abcc4b318bcedc8e9f2674fb
adithyaux <adithyaananth2204@gmail.com>
2021-06-17 19:44:02 +0530

    search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
+import{Form,Card,Image,Icon} from 'semantic-ui-react';
 import './App.css';
 
 function App() {
+
+  const[name,setName] = useState('');
+  const[userName,setUsernameName] = useState('');
+  const[followers,setFollowers] = useState('');
+  const[following,setfollowing] = useState('');
+  const[repos,setRepos] = useState('');
+  const[avatar,setAvatar] = useState('');
+  const[userInput,setUserinput] = useState('');
+  const[error,setError] = useState('null');
+
+  useEffect(()=>{
+    fetch('https://api.github.com/users/example')
+    .then(res=>res.json())
+    .then(data=>{
+      setData(data);
+    });
+
+  },[]);
+
+  const setData=({name,login,followers,following,
+    public_repos,avatar_url})=>{
+      setName(name);
+      setUsernameName(login);
+      setFollowers(followers);
+      setfollowing(following);
+      setRepos(public_repos);
+      setAvatar(avatar_url);
+
+
+  };
+
+  const handleSearch = (e)=>{
+    setUserinput(e.target.value)
+  }
+
+  const handleSubmit=()=>{
+    fetch(`https://api.github.com/users/${userInput}`)
+    .then(res=>res.json())
+    .then(data=>{
+      if(data.message){
+        setError(data.message)
+      }else{
+      setData(data);
+      setError(null);
+      }
+    });
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="navbar">Github Search</div>
+    <div className="search">
+      <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              placeholder='github user'
+              name='github user'
+              value={name}
+              onChange={handleSearch}
+            />
+            
+            <Form.Button content='Search' />
+          </Form.Group>
+        </Form>
     </div>
+    {error ?(<h1>{error}</h1>) :(<div className="card">
+        <Card>
+          <Image src={avatar} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Header>{userName}</Card.Header>
+            
+            {/* <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              Matthew is a musician living in Nashville.
+            </Card.Description> */}
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              {followers} Followers
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              {repos} Repos
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              {following} Following
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              22 Friends
+            </a>
+          </Card.Content>
+        </Card>
+
+    </div>) }
+    
+      
+    </div>
+    
   );
 }
 
